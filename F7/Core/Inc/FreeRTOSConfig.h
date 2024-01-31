df9be86d26d8660e7d0b27c82a2b93709fc04f9f
Holden <htramsey98@gmail.com>
2024-01-31 06:44:50 -0500

    add newlib safety

--- F7/Core/Inc/FreeRTOSConfig.h
+++ F7/Core/Inc/FreeRTOSConfig.h
@@ -30,7 +30,7 @@ extern uint32_t SystemCoreClock;
 #define configUSE_COUNTING_SEMAPHORES           1
 #define configQUEUE_REGISTRY_SIZE               5
 #define configENABLE_BACKWARD_COMPATIBILITY     0
-#define configUSE_NEWLIB_REENTRANT              0
+#define configUSE_NEWLIB_REENTRANT              1
 #define configUSE_MPU_WRAPPERS_V1               0
 #define configUSE_PICOLIBC_TLS                  0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
